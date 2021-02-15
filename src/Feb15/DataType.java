342adf973f9ff213956a576d7578099e80b64a1b
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2021-02-15 23:34:03 +0530

    Initial

--- src/Feb15/DataType.java
+++ src/Feb15/DataType.java
@@ -2,12 +2,13 @@ package Feb15;
 
 public class DataType
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
     char ch='a';
     int  x= ch ;
 
     System.out.println(x);
-}
+    }
 }
 
 

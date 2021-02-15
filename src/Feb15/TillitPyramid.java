1669831479e842d06ea7b2755398656f94f07252
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2021-02-15 23:23:04 +0530

    Initial

---
+++ src/Feb15/TillitPyramid.java
@@ -0,0 +1,26 @@
+package Feb15;
+
+public class TillitPyramid {public static void main(String[] args) {
+    int n = 5;
+    for (int i= 0; i<= n-1 ; i++)
+    {
+        for (int j=0; j<=i; j++)
+        {
+            System.out.print(i+1 +" ");
+            if(j!=i )
+                System.out.print("* ");
+        }
+        System.out.println("");
+    }
+    for (int i=n-1; i>=0; i--)
+    {
+        for(int j=0; j <= i-1;j++)
+        {
+            System.out.print(i+" ");
+            if(j!=i-1)
+                System.out.print("* ");
+        }
+        System.out.println("");
+    }
+}
+}

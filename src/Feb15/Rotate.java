ea329373a891706f892e20b2da0efc0efa17eecc
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2021-02-15 23:34:31 +0530

    Initial

--- src/Feb15/Rotate.java
+++ src/Feb15/Rotate.java
@@ -2,7 +2,9 @@ package Feb15;
 
 import java.util.Scanner;
 
-public class Rotate { public static void main(String[] args) {
+public class Rotate
+{
+    public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int n1=sc.nextInt();
     int rot=sc.nextInt();

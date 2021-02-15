af99557a351cce096fd5766a8d07476e14e47bc1
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2021-02-15 23:26:19 +0530

    Initial

---
+++ src/Feb15/NoSquareWithDiagonal.java
@@ -0,0 +1,22 @@
+package Feb15;
+
+public class NoSquareWithDiagonal {
+    public static void main(String[] args) {
+        int x,a; x=5;
+        a=1;
+        for(int i=1;i<=x;i++)
+        {
+            for(int j=x;j>=1;j--)
+            {   if(i==a && j==a)
+            {
+                System.out.print("*");
+            }
+            else
+                System.out.print(j);
+            }
+            System.out.println();
+
+            a++;
+        }
+    }
+}

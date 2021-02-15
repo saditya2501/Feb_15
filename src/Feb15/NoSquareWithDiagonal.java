e5a9d1f8aa78887e7cb7f8d642623141d06f443d
saditya2501 <aditya.chuahan_cs18@gla.ac.in>
2021-02-15 23:26:48 +0530

    Initial

--- src/Feb15/NoSquareWithDiagonal.java
+++ src/Feb15/NoSquareWithDiagonal.java
@@ -1,7 +1,8 @@
 package Feb15;
-
-public class NoSquareWithDiagonal {
-    public static void main(String[] args) {
+public class NoSquareWithDiagonal
+{
+    public static void main(String[] args)
+    {
         int x,a; x=5;
         a=1;
         for(int i=1;i<=x;i++)
